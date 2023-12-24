ebc46cdb064c036bce4b5f047f09f80a1c422386
Hulkar Sharif <hulkar.sharif@gmail.com>
2023-12-23 21:04:08 -0500

    Create global variables

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-    return;
-}
+import { useEffect } from "react";
+import { admin } from "./api";
+import { Typography, TypographyProps } from "./design-system";
+
+const App = () => {
+    useEffect(() => {
+        admin
+            .forgotPassword("sharipovah01@gmail.com")
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+    return (
+        <>
+            <Typography variant={TypographyProps.variant.h2}>Hello</Typography>
+            <Typography variant={TypographyProps.variant.subtitleLg}>
+                Hello
+            </Typography>
+        </>
+    );
+};
 
 export default App;
---
+++ src/design-system/Typography/Typography.js
@@ -0,0 +1,49 @@
+/* Variant: h1, h2, h3, h4, h5, h6, p-sm, p-md, p-lg */
+/* Weight: normal, medium, semibold, bold */
+/* Align: "center" | "left" | "right" | "justify" | "inherit";*/
+
+import {
+    variantClassNames,
+    weightClassNames,
+    alignClassNames
+} from "./classnames";
+
+export const Typography = ({ variant, weight, align, className, children }) => {
+    const variantClassName = variantClassNames[variant];
+    const weightClassName = weightClassNames[weight] || "";
+    const alignClassName = alignClassNames[align] || "";
+
+    const finalClassName =
+        `${variantClassName} ${weightClassName} ${alignClassName} ${
+            className || ""
+        }`.trim();
+
+    if (
+        variant === "displayLG" ||
+        variant === "displaySM" ||
+        variant === "h1"
+    ) {
+        return <h1 className={finalClassName}>{children}</h1>;
+    } else if (variant === "h2") {
+        return <h2 className={finalClassName}>{children}</h2>;
+    } else if (variant === "h3") {
+        return <h3 className={finalClassName}>{children}</h3>;
+    } else if (variant === "h4") {
+        return <h4 className={finalClassName}>{children}</h4>;
+    } else if (variant === "h5") {
+        return <h5 className={finalClassName}>{children}</h5>;
+    } else if (variant === "h6") {
+        return <h6 className={finalClassName}>{children}</h6>;
+    } else if (
+        variant === "paragraphLG" ||
+        variant === "paragraphMD" ||
+        variant === "paragraphSM" ||
+        variant === "subtitleLG" ||
+        variant === "subtitleMD" ||
+        variant === "subtitleSM"
+    ) {
+        return <p className={finalClassName}>{children}</p>;
+    }
+
+    return <h1>{children}</h1>;
+};
