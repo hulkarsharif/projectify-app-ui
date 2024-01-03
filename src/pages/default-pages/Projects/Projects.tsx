fb2f97e9fe9d3059e8bd82e436957b665e4ea631
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-01-02 19:09:06 -0500

    Create stories,projects,teammember

---
+++ src/pages/default-pages/Projects/Projects.tsx
@@ -0,0 +1,40 @@
+import { DefaultPageWrapper } from "../../components";
+import { MainLayout } from "../../components";
+
+import "./Projects.css";
+import lock from "../../../assets/images/lock.jpg";
+
+const projects = [
+    {
+        name: "Project Management Tool",
+        description:
+            "A project management tool for tracking tasks, assigning team members, and monitoring project progress."
+    },
+    {
+        name: "Bug Tracking System",
+        description:
+            "A system for tracking and managing software bugs, including features for bug reporting, assignment, and resolution."
+    },
+    {
+        name: "Feature Request Tracker",
+        description:
+            "An application for collecting and prioritizing feature requests from users, allowing the development team to plan future releases."
+    }
+];
+const Projects = () => {
+    return (
+        <MainLayout>
+            {!projects ? (
+                <DefaultPageWrapper
+                    imagePath={adminguy}
+                    pageTitle="You don't have any projects yet!"
+                    btnText="Add a Task"
+                />
+            ) : (
+                "Hello"
+            )}
+        </MainLayout>
+    );
+};
+
+export { Projects };
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,7 @@
 export * from "./admin-auth/sign-up/Signup";
 export * from "./admin-auth/login/Login";
 export * from "./admin-auth/forgot-password/ForgotPassword";
+export * from "./default-pages/Projects/Projects";
+export * from "./default-pages/personal-tasks/PersonalTasks";
 export * from "./admin-auth/update-Password/UpdatePassword";
+export * from "./default-pages/team-member/TeamMember";
