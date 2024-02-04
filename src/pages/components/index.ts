b8376819af9b6b03e4d639948595171763f82535
Hulkar Sharif <hulkar.sharif@gmail.com>
2024-02-04 11:57:59 -0500

    Refactor kanbancard

--- src/pages/components/index.ts
+++ src/pages/components/index.ts
@@ -3,5 +3,5 @@ export * from "./SideBarUser";
 export * from "./AppLayout";
 export * from "./NoDataPlaceHolder";
 export * from "./AuthActionLink";
-export * from "./KanbanCard";
+export * from "./AuthActionLink";
 export * from "./ConfirmationModal";
